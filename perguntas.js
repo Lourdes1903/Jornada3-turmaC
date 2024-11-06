bad3de23bc24d2acf6fcb716406bc6b631b18ff4
Lourdes1903 <126637277+Lourdes1903@users.noreply.github.com>
2024-11-06 20:41:39 -0300

    06 11 2024 ok

--- perguntas.js
+++ perguntas.js
@@ -19,3 +19,50 @@ criaCartao(
     "Qual a definição do Teorema de Pitágoras?",
     "Em um triângulo retângulo a hipotenusa ao quadrado é igual a soma dos catetos ao quadrado."
 )
+
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O HTML é uma linguagem de marcação.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a função do oxigênio no nosso organismo?',
+    'A produção de energia.'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o polígono que tem nove lados?',
+    'Eneágono.'
+)
+
+criaCartao(
+    'Física',
+    'Qual é a terceira Lei de Newton?',
+    'Para toda ação tem uma reação.'
+)
+
+criaCartao(
+    'Arte',
+    'Quais são as cores primárias?',
+    'Vermelho, amarelo e azul'
+)
+criaCartao(
+    'Matemática',
+    'O que é um triângulo equilátero?',
+    'É um triângulo que possui todos os lados congruentes, ou seja, com a mesma medida.'
+)
+
+criaCartao(
+    'Química',
+    'Quantas ligações faz o elemento Carbono?',
+    'Quatro ligações'
+)
+
+criaCartao(
+    'Química',
+    'Qual é a fórmula da água?',
+    'H20'
+)
