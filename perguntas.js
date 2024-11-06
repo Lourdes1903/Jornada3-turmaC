685057b3bd44b10296a81cc5d45b3cae989e2f95
Lourdes1903 <126637277+Lourdes1903@users.noreply.github.com>
2024-11-06 12:26:33 -0300

    06 11 2024

---
+++ perguntas.js
@@ -0,0 +1,21 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação.'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função',
+    'Uma função é um bloco de código que executa uma tarefa'
+)
+
+criaCartao(
+    "Matemática",
+    "Qual a definição do Teorema de Pitágoras?",
+    "Em um triângulo retângulo a hipotenusa ao quadrado é igual a soma dos catetos ao quadrado."
+)
